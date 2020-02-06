b90b6955abe847e340b7a61136fc731af214b3dd
Micah Schuster <micahdschuster@gmail.com>
2020-02-06 17:36:53 -0500

    Initial commit

---
+++ Assets/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    ParticleSystem[] ps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            for(int i = 0; i < ps.Length; i++)
+            {
+                ps[i].Play();
+            }
+        }
+    }
+}
