c5f9362d3aa5dbf97404389550525064e18d969f
Micah Schuster <schusterm@wit.edu>
2020-09-25 19:43:54 -0400

    Add VFX Graph Scene

---
+++ Assets/VfxRotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VfxRotate : MonoBehaviour
+{
+
+    Rigidbody rb;
+    float durationTime = 10f;
+    Vector3 direction = new Vector3(0f, 0f, 1f);
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = new Vector3(0f, 0f, 200f);
+        Debug.Log(rb.angularVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
